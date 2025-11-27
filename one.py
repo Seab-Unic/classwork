f91352992ec15f21adc0d1dd680e574f1a734a12
Seaborgium_element <evskv42@yandex.ru>
2025-11-27 18:01:52 +0700

    Update one.py

--- one.py
+++ one.py
@@ -1,9 +1,6 @@
-def pnnb(n, b=1):
-    if b > n:
-        return
-    print(f"{b}")
-    pnnb(n, b + 1)
+n = [65, 77537, 123]
+for num in n:
+    result = "четное" if num % 2 == 0 else "нечетное"
+    print(f"Число {num} — {result}")
 
-n = int(input())
-pnnb(n)
 
