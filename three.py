283bb8f82e9fa63fff25a21cc4b3548d60df87cf
Seaborgium_element <evskv42@yandex.ru>
2025-12-03 22:05:20 +0700

    Update three.py

--- three.py
+++ three.py
@@ -1,8 +1,4 @@
-def s(n):
-    if n == 1:
-        return "да"
-    if n <= 0 or n % 2 != 0:
-        return "нет"
-    return s(n // 2)
-n = int(input())
-print(s(n))
+list = [5, 12, 8, 20, 3, 15]
+gen = (x if x > 10 else 0 for x in list)
+for x in gen:
+    print(x)
