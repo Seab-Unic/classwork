4af1ac855e0afd438d7d39581b7f35b3471d41b7
Seaborgium_element <evskv42@yandex.ru>
2025-12-03 22:21:04 +0700

    Update two.py

--- two.py
+++ two.py
@@ -1,3 +1,4 @@
 fizzbuzz = ["FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else "" for i in range(1, 101)]
-result = [x for x in fizzbuzz if x]
-print(result)
+for x in fizzbuzz:
+    print(x)
+
